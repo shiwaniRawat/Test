939bcd3c5f618204f30a7e349ccfb60ff274e72e
shiwani rawat <shivirwt@gmail.com>
2018-07-04 12:00:35 +0530

    updated

---
+++ app/src/main/java/com/example/gb/test/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.gb.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText studentName,studentPhone,studentAddress,studentCollege;
+    Button addStudent,displayStudent;
+    TextView displaystudent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.arraylist);
+        String collegeName;
+    }
+}
